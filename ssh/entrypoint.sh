f431e479d9ed0e6c05626351f6127badadc4ae10
Jon Maddox <jon@jonmaddox.com>
2018-10-22 15:08:51 -0400

    change it up

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -7,7 +7,8 @@ SSH_PATH="/github/home/.ssh"
 echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
-chmod 600 "$SSH_PATH/*"
+chmod 600 "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key.pub"
 
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
